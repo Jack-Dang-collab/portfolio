b4921c86ccb45d1eec318eb99926dd8efd61da8d
Jack-Dang-collab <dangthanhtphcm95@gmail.com>
2023-05-07 03:11:55 -0400

    Change About paragraphs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,16 +5,16 @@ const About = () => {
     <section className='pt-[25px] pb-[25px]'>
         <h2 className='text-[42px] my-[25px] mx-auto text-center items-center justify-center font-bold'>About Me</h2>
         <p className='indent-8 text-[18px] leading-tight max-w-[75ch] text-justify items-center justify-center mx-auto font-semibold px-10'>
-          Hello and welcome to my portfolio! My name is Thanh, and I am a self-taught blockchain developer with a passion for building innovative decentralized applications. My journey into blockchain began out of a curiosity for how this emerging technology could transform traditional systems and industries.
+          Welcome to my portfolio! I'm Thanh, a self-taught blockchain developer who's passionate about creating innovative decentralized applications. I became intrigued by blockchain's potential to revolutionize traditional systems and industries, and I've since built a range of exciting projects.
         </p>
         <p className='indent-8 text-[18px] leading-tight max-w-[75ch] text-justify items-center justify-center mx-auto font-semibold px-10'>
-          One of my most exciting projects involved building a crowdfunding platform on the blockchain. This platform allowed users to contribute to various campaigns in a secure and transparent manner, leveraging the power of smart contracts to ensure that funds were distributed fairly and as per the campaign's conditions.
+          For instance, I built a crowdfunding platform that uses smart contracts to ensure secure, transparent, and fair distribution of funds. Additionally, I created a horoscope application that provides personalized astrological readings to users. By utilizing the RapidAPI, I was able to display relevant astrological data in a user-friendly interface.
         </p>
         <p className='indent-8 text-[18px] leading-tight max-w-[75ch] text-justify items-center justify-center mx-auto font-semibold px-10'>
-          Another project I worked on was a horoscope application that delivered personalized astrological readings to users. I utilized the RapidAPI to fetch relevant astrological data and displayed it in a user-friendly interface that made the application accessible to everyone.
+          My latest project is a web3 marketplace, built with Solidity, that allows buyers and sellers to conduct transactions directly, without the need for a third party. This enhances the security, transparency, and efficiency of transactions, and I'm always looking for new ways to utilize blockchain technology to benefit society.
         </p>
         <p className='indent-8 text-[18px] leading-tight max-w-[75ch] text-justify items-center justify-center mx-auto font-semibold px-10'>
-          My most recent project is a web3 marketplace built using Solidity. This platform enables sellers and buyers to sell and buy goods without a third party to monitor, making transactions more secure, transparent, and efficient. I am constantly seeking new ways to explore the potential of blockchain technology and how it can benefit society.
+          I'm also excited to share my latest creation - an AI summarizer that leverages RapidAPI, Redux, ReactJS, and TailwindCSS to summarize any article or webpage. By extracting key points and important information, users can quickly obtain the insights they need to save time and improve their productivity. I'm thrilled about the potential impact this project can have on individuals and businesses alike.
         </p>
         <p className='indent-8 text-[18px] leading-tight max-w-[75ch] text-justify items-center justify-center mx-auto font-semibold px-10'>
           If you are looking for a self-motivated and innovative blockchain developer who can bring your ideas to life, then I would love to hear from you.
