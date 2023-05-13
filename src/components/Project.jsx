a964e0e97e924232686d9147433baa19dd962d28
Jack-Dang-collab <dangthanhtphcm95@gmail.com>
2023-05-12 21:27:11 -0400

    Fix typo

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ const Project = ({ name, image, website, githubSite }) => {
   return (
     <div className="hover:-translate-y-1 duration-300 max-w-[350px] p-5 border border-solid border-[#0088BB] rounded-lg bg-gray-900 bg-opacity-50">
       <h3 className='text-[32px] font-semibold mb-[10px]'>{name}</h3>
-      <img src={image} alt="Crowdfunding Landing Page" />
+      <img src={image} alt={name} />
 
       <a href={website} target="_blank" className="my-[10px] mx-[5px] mb-0 border bg-cyan-800 hover:bg-cyan-500 text-white p-2 rounded-md inline-block font-semibold">Site</a>
       <a href={githubSite} target="_blank" className="my-[10px] mx-[5px] mb-0 border bg-cyan-800 hover:bg-cyan-500 text-white p-2 rounded-md inline-block font-semibold">Code</a>
