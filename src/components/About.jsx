2c2441ed5a624e27500586c401c0db017e25a2c5
Jack-Dang-collab <dangthanhtphcm95@gmail.com>
2023-05-19 23:53:57 -0400

    Update About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,13 +8,16 @@ const About = () => {
           Hello, my name is Thanh, and I am a self-taught front-end and blockchain developer. I have always been interested in technology and coding, and I have spent the last few years building my skills in these areas.
         </p>
         <p className='indent-8 text-[18px] leading-tight max-w-[75ch] text-justify items-center justify-center mx-auto font-semibold px-10'>
-          In terms of front-end development, I have experience with HTML, CSS, JavaScript, ReactJS, Redux, and TailwindCSS. I enjoy creating user interfaces that are intuitive, responsive, and visually appealing. I have built several projects that showcase my skills, including a crowdfunding platform, a web3 market platform, horoscope, URL summarizer and a portfolio website.
+          In terms of front-end development, I have experience with HTML, CSS, JavaScript, ReactJS, Redux, and TailwindCSS. I enjoy creating user interfaces that are intuitive, responsive, and visually appealing. I have built several projects that showcase my skills, including a crowdfunding platform, a web3 market platform, horoscope, URL summarizer, and a portfolio website.
         </p>
         <p className='indent-8 text-[18px] leading-tight max-w-[75ch] text-justify items-center justify-center mx-auto font-semibold px-10'>
-          In addition to front-end development, I am also interested in blockchain technology. I have experience with Ethereum, Solidity, and Thirdweb framework, and I have built several decentralized applications (DApps) that leverage smart contracts to provide secure and transparent functionality. I believe that blockchain has the potential to revolutionize many industries, and I am excited to be a part of this emerging field.
+          One of my notable projects is an Airbnb clone, where I leveraged the power of React, TailwindCSS, Prisma, MongoDB, and NextAuth to create a fully functional platform. This project aimed to replicate the core features of the popular Airbnb website, allowing users to browse and book accommodations, and manage reservations. I implemented features such as search functionality, user authentication, and listing creation.
         </p>
         <p className='indent-8 text-[18px] leading-tight max-w-[75ch] text-justify items-center justify-center mx-auto font-semibold px-10'>
-          Overall, I am a dedicated and passionate developer who is always looking for new challenges and opportunities to learn and grow. I am confident that my skills and experience would make me a valuable asset to any team, and I am eager to contribute my expertise to help create innovative solutions that make a positive impact on people's lives.
+          In addition to front-end development, I am also passionate about blockchain technology. I have experience with Ethereum, Solidity, and the Thirdweb framework, and I have built several decentralized applications (DApps) that leverage smart contracts to provide secure and transparent functionality.
+        </p>
+        <p className='indent-8 text-[18px] leading-tight max-w-[75ch] text-justify items-center justify-center mx-auto font-semibold px-10'>
+          Overall, I am a dedicated and passionate developer who is always looking for new challenges and opportunities to learn and grow. I believe that my skills and experience in front-end development, blockchain technology, and building projects like the Airbnb clone make me a valuable asset to any team. I am eager to contribute my expertise and collaborate with like-minded individuals to create innovative solutions that make a positive impact on people's lives.
         </p>
     </section>
   )
